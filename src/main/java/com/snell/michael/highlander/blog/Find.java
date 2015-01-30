578417174fbc2e87962d1adc95314d6ad0bd1577
Michael Snell <michael@snell.com>
2015-01-30 15:38:40 +0000

    Adding only

--- src/main/java/com/snell/michael/highlander/blog/Find.java
+++ src/main/java/com/snell/michael/highlander/blog/Find.java
@@ -45,9 +45,7 @@ public class Find {
     }
 
     public Person findOnlyStream(String email, Collection<Person> people) {
-        return people.stream()
-            .filter(p -> email.equalsIgnoreCase(p.getEmail()))
-            .collect(only());
+        return only(people.stream().filter(p -> email.equalsIgnoreCase(p.getEmail())));
     }
 
     private Predicate<Person> hasEmail(String email) {
